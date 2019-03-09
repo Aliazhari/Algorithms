b461a3d8a02e84be2ab8eea5e9b4bd665a16ca0a
Aliazhari <Aliazhari@hotmail.com>
2019-03-09 12:21:33 -0500

    Recursion

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Factorial {
+
+	public static int Fact(int n) throws IllegalArgumentException {
+		
+		if (n < 0) throw new IllegalArgumentException();
+		
+		if (n == 0) return 1;
+		
+		return (n * Fact(n - 1));
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 11; i++)
+		System.out.println("Factorial of " + i + " :" + Fact(i));
+
+	}
+
+}
