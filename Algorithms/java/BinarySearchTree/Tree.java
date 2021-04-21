a872d76d2dc7ea6e14acad52e2a64dfe21c5a2ba
Aliazhari <Aliazhari@hotmail.com>
2021-04-20 21:09:59 -0400

    adding python recursion

---
+++ Algorithms/java/BinarySearchTree/Tree.java
@@ -0,0 +1,26 @@
+package BinarySearchTree;
+
+import java.util.Iterator;
+
+/**
+ * @File Tree.java
+ * 
+ * @author Ali Azhari
+
+ * @param <E>
+ */
+public interface Tree<E> extends Iterable<E>{
+	
+	Position<E> root();
+	Position<E> parent(Position<E> p) throws IllegalArgumentException;
+	Iterable<Position<E>> children(Position<E> p) throws IllegalArgumentException;
+	int numChildren(Position<E>p) throws IllegalArgumentException;
+	boolean isExternal(Position<E> p) throws IllegalArgumentException;
+	boolean isInternal(Position<E> p) throws IllegalArgumentException;
+	boolean isRoot(Position<E> p) throws IllegalArgumentException;
+	int size();
+	boolean isEmpty();
+	Iterator<E> iterator();
+	Iterable<Position<E>> positions();
+
+}
